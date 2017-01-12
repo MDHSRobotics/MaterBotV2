d48dd5654ecfcae5242026459c322c27c4aa079f
CAMPSMITH <CAMPSMITH@users.noreply.github.com>
2017-01-12 10:26:56 -0800

    Update OI.java
    added test move commands

--- src/org/usfirst/frc/team4141/robot/OI.java
+++ src/org/usfirst/frc/team4141/robot/OI.java
@@ -6,6 +6,8 @@ import org.usfirst.frc.team4141.MDRobotBase.MDJoystick;
 import org.usfirst.frc.team4141.MDRobotBase.MDRobotBase;
 import org.usfirst.frc.team4141.MDRobotBase.OIBase;
 import org.usfirst.frc.team4141.MDRobotBase.RioHID;
+import org.usfirst.frc.team4141.robot.commands.MDMoveCommand;
+import org.usfirst.frc.team4141.robot.commands.MDMoveCommand.Direction;
 import org.usfirst.frc.team4141.robot.commands.MDPrintCommand;
 
 
@@ -25,10 +27,24 @@ public class OI extends OIBase{
 		//A robot should have 1 or more operator interfaces (OI)
 		//Typically, a robot will have at least 1 joystick and 1 console
 		
+		//if using gamepad, the buttons are generally as follows:
+		// x is button 1
+		// a is button 2
+		// b is button 3
+		// y is button 4
+		
+		
+		
 		//Configure the joystick(s) here
 		add(new MDJoystick(getRobot(), "joystick", 0)
-			.whenPressed("A",3,new MDPrintCommand(getRobot(),"A Command","A Command message"))
+			.whenPressed("rightBumper",5,new MDPrintCommand(getRobot(),"Right Bumper Command","Right Bumper Command message"))
 			.whileHeld("leftBumper",6,new MDPrintCommand(getRobot(),"Left Bumper Command","Left Bumper Command message"))
+		    //the following commands are test move commands useful in testing drive configuration and set up
+		    //comment out and replace as needed
+			.whenPressed("X",1,new MDMoveCommand(getRobot(),"left command",Direction.left))
+			.whenPressed("A",2,new MDMoveCommand(getRobot(),"reverse command",Direction.reverse))
+			.whenPressed("B",3,new MDMoveCommand(getRobot(),"right command",Direction.right))
+			.whenPressed("Y",4,new MDMoveCommand(getRobot(),"forward command",Direction.forward))
 			.configure()
 		);
 
