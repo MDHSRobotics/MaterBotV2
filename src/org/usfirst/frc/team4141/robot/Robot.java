ab64ccc92ef03e434ef40f79e67ba729337b4dd4
CAMPSMITH <CAMPSMITH@users.noreply.github.com>
2017-01-12 09:36:31 -0800

    Update Robot.java
    Defaulting Robot configuration to mecanum drive

--- src/org/usfirst/frc/team4141/robot/Robot.java
+++ src/org/usfirst/frc/team4141/robot/Robot.java
@@ -1,4 +1,3 @@
-
 package org.usfirst.frc.team4141.robot;
 
 
@@ -49,14 +48,13 @@ public class Robot extends MDRobotBase {
 		//A robot is composed of subsystems
 		//A robot will typically have 1 drive system and several other fit to purpose subsystems		
 		//The Drive system is a special subsystem in that it has specific logic handle the speed controllers
-		add(new MDDriveSubsystem(this, "driveSystem", Type.TankDrive)
-				.add(MotorPosition.left, new Victor(0))
-				.add(MotorPosition.right, new Victor(1))
+		add(new MDDriveSubsystem(this, "driveSystem", Type.MecanumDrive)
+				.add(MotorPosition.frontRight, new Victor(0))
+				.add(MotorPosition.rearRight, new Victor(1))
+				.add(MotorPosition.frontLeft, new Victor(2))
+				.add(MotorPosition.rearLeft, new Victor(3))
 				.add("accelerometer", new MD_BuiltInAccelerometer())
 				.add("IMU", new MD_IMU())
-				.add("a", new DoubleConfigSetting(0.0, 1.0, 0.25))
-				.add("b", new DoubleConfigSetting(0.0, 1.0, 0.4))
-				.add("c", new DoubleConfigSetting(0.0, 1.0, 1.0))
 				.configure()
 		);	
 
