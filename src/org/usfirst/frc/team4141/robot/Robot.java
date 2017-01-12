9923cd91ad795b654a7b2eff63d231303aef9a40
CAMPSMITH <CAMPSMITH@users.noreply.github.com>
2017-01-12 09:58:38 -0800

    Update Robot.java
    added examples for each drive system and improved comments

--- src/org/usfirst/frc/team4141/robot/Robot.java
+++ src/org/usfirst/frc/team4141/robot/Robot.java
@@ -48,6 +48,9 @@ public class Robot extends MDRobotBase {
 		//A robot is composed of subsystems
 		//A robot will typically have 1 drive system and several other fit to purpose subsystems		
 		//The Drive system is a special subsystem in that it has specific logic handle the speed controllers
+		//We have 2 types of drive systems, tank drive and mecanum drive
+		//uncomment the desired drive system and adjust the motor configuration as needed
+		//Mecanum example :
 		add(new MDDriveSubsystem(this, "driveSystem", Type.MecanumDrive)
 				.add(MotorPosition.frontRight, new Victor(0))
 				.add(MotorPosition.rearRight, new Victor(1))
@@ -57,6 +60,26 @@ public class Robot extends MDRobotBase {
 				.add("IMU", new MD_IMU())
 				.configure()
 		);	
+		
+		//TankDrive with 2 motors example:
+//		add(new MDDriveSubsystem(this, "driveSystem", Type.TankDrive)
+//				.add(MotorPosition.right, new Victor(0))
+//				.add(MotorPosition.left, new Victor(1))
+//				.add("accelerometer", new MD_BuiltInAccelerometer())
+//				.add("IMU", new MD_IMU())
+//				.configure()
+//		);	
+		//TankDrive with 4 motors example:
+//		add(new MDDriveSubsystem(this, "driveSystem", Type.TankDrive)
+//				.add(MotorPosition.frontRight, new Victor(0))
+//				.add(MotorPosition.rearRight, new Victor(1))
+//				.add(MotorPosition.frontLeft, new Victor(2))
+//				.add(MotorPosition.rearLeft, new Victor(3))
+//				.add("accelerometer", new MD_BuiltInAccelerometer())
+//				.add("IMU", new MD_IMU())
+//				.configure()
+//		);	
+		
 
 	}
 
